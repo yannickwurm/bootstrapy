9ded745ee8c521ae5e812aed819197c3ef490754
Yannick Wurm <y.wurm@qmul.ac.uk>
2013-09-18 09:49:32 +0100

    rescue formatting errors

--- _rubyscholar/rubyscholar.rb
+++ _rubyscholar/rubyscholar.rb
@@ -27,14 +27,14 @@ class RubyScholar
       STDERR << "Found #{papers.length} papers.\n"
       papers.each do |paper|
         paperDetails   = paper.css("#col-title")
-        title          = paperDetails[0].children[0].content.clean
-        googleUrl      = paperDetails[0].children[0].attribute('href')
-        authors        = paperDetails[0].children[2].content.clean
+        title          = paperDetails[0].children[0].content.clean  rescue ''
+        googleUrl      = paperDetails[0].children[0].attribute('href') rescue ''
+        authors        = paperDetails[0].children[2].content.clean rescue ''
         authors.gsub!("...", "et al")
 
-        journal        = paperDetails[0].children[4].content
-        journalName    = journal.split(/,|\d/).first.clean
-        journalDetails = journal.gsub(journalName, '').clean
+        journal        = paperDetails[0].children[4].content rescue '' 
+        journalName    = journal.split(/,|\d/).first.clean   rescue ''
+        journalDetails = journal.gsub(journalName, '').clean 
 
         year           = paper.css("#col-year").text # is the last thing we get
 
@@ -81,50 +81,52 @@ class RubyScholar
     def to_html
       ##@doc = Nokogiri::HTML::DocumentFragment.parse "" 
       builder = Nokogiri::HTML::Builder.new do |doc|
-        doc.div( :class => "publication") {
-          @parser.parsedPapers.each_with_index { |paper, index|
-            doc.li( :value=> ( (@parser.parsedPapers).length - index).to_s)  {
-              
-              doc.b    paper[:title] + '.'
-              doc.text ' (' + paper[:year] + '). '
-              
-              if paper[:authors].include?(@nameToHighlight)
-                doc.text( paper[:authors].sub(Regexp.new(@nameToHighlight + '.*'), '') )
-                doc.span( :class => "label") { doc.text @nameToHighlight }
-                doc.text( paper[:authors].sub(Regexp.new('.*' + @nameToHighlight), '') )
-              else
-                doc.text( paper[:authors]) + '.'
-              end
+	  doc.div( :class => "publication") {
+            doc.ol {	
+	        @parser.parsedPapers.each_with_index { |paper, index|
+                doc.li( :value=> ( (@parser.parsedPapers).length - index).to_s)  {
 
-              doc.em   ' ' + paper[:journalName]
-              doc.text ' ' + paper[:journalDetails]
-              unless paper[ :doi].empty?
-                doc.text(' ')
-                doc.a( :href => URI.join("http://dx.doi.org/", paper[ :doi]))  { 
-                  doc.text "[DOI]" 
-                } 
-              end
-              if @pdfLinks.keys.include?(paper[:title])
-                doc.text(' ')
-                doc.a( :href => @pdfLinks[paper[:title]])  { 
-                  doc.text "[PDF]"
-                } 
-              end
-              if paper[ :citationCount].to_i > @minCitations
-                doc.text(' ')
-                doc.a( :href => paper[ :citingPapers], :title => "Citations") { 
-                  doc.span( :class => "badge badge-inverse") { doc.test("#{paper[ :citationCount]}x") }
-                } 
-              end
-              if altmetricDOIs.include?( paper[ :doi])
-                doc.text(' ')
-                doc.span( :class                => 'altmetric-embed',
-                          :'data-badge-popover' => 'bottom',
-                          :'data-doi'           => paper[ :doi]        )
-              end
+                  doc.b    paper[:title] + '.'
+                  doc.text ' (' + paper[:year] + '). '
+
+                  if paper[:authors].include?(@nameToHighlight)
+                    doc.text( paper[:authors].sub(Regexp.new(@nameToHighlight + '.*'), '') )
+                    doc.span( :class => "label") { doc.text @nameToHighlight }
+                    doc.text( paper[:authors].sub(Regexp.new('.*' + @nameToHighlight), '') )
+                  else
+                    doc.text( paper[:authors]) + '.'
+                  end
+
+                  doc.em   ' ' + paper[:journalName]
+                  doc.text ' ' + paper[:journalDetails]
+                  unless paper[ :doi].empty?
+                    doc.text(' ')
+                    doc.a( :href => URI.join("http://dx.doi.org/", paper[ :doi]))  { 
+                      doc.text "[DOI]" 
+                    } 
+                  end
+                  if @pdfLinks.keys.include?(paper[:title])
+                    doc.text(' ')
+                    doc.a( :href => @pdfLinks[paper[:title]])  { 
+                      doc.text "[PDF]"
+                    } 
+                  end
+                  if paper[ :citationCount].to_i > @minCitations
+                    doc.text(' ')
+                    doc.a( :href => paper[ :citingPapers], :title => "Citations") { 
+                      doc.span( :class => "badge badge-inverse") { doc.test("#{paper[ :citationCount]}x") }
+                    } 
+                  end
+                  if altmetricDOIs.include?( paper[ :doi])
+                    doc.text(' ')
+                    doc.span( :class                => 'altmetric-embed',
+                              :'data-badge-popover' => 'bottom',
+                              :'data-doi'           => paper[ :doi]        )
+                  end
+                }
+              }
+	      }
             }
-          }
-        }
       end
       return builder.to_html
     end
